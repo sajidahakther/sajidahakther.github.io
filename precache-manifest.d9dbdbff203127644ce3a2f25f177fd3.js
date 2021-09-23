5c7b1e4a0b0ed64bb352e622e6a457b52a16d85d
Sajidah Akther <sajidahakther@googlemail.com>
2021-09-23 20:12:33 +0100

    Updates

---
+++ precache-manifest.d9dbdbff203127644ce3a2f25f177fd3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc5087e1040bf942654b1776d88ec4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef546e6c8fafcabc3865",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "950e1cc2e12e97267ced",
+    "url": "/static/css/main.34b37f9f.chunk.css"
+  },
+  {
+    "revision": "ef546e6c8fafcabc3865",
+    "url": "/static/js/2.6650cdf2.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/static/js/2.6650cdf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950e1cc2e12e97267ced",
+    "url": "/static/js/main.5015ebcb.chunk.js"
+  },
+  {
+    "revision": "38ff605c96a961a6412d",
+    "url": "/static/js/runtime-main.705dec9d.js"
+  },
+  {
+    "revision": "64a8711f9395e60ada4006943e11eed9",
+    "url": "/static/media/Apercu-Light.64a8711f.otf"
+  },
+  {
+    "revision": "3d6dd91c6e9ab1f8b42c4ac4918eabde",
+    "url": "/static/media/Canela-Light.3d6dd91c.woff"
+  },
+  {
+    "revision": "0249a3df988f210370bde52ae4a6add3",
+    "url": "/static/media/arrow.0249a3df.svg"
+  },
+  {
+    "revision": "2612b99fa9c0fa5120e5d30114924d3f",
+    "url": "/static/media/mobile-close.2612b99f.svg"
+  },
+  {
+    "revision": "39c559bf7a829464d3eaffd42b3edca4",
+    "url": "/static/media/mobile-menu.39c559bf.svg"
+  },
+  {
+    "revision": "1aca2fa7640214e22b7446c4edb0d489",
+    "url": "/static/media/preview.1aca2fa7.png"
+  },
+  {
+    "revision": "7778201959466993454f01ee62f053d1",
+    "url": "/static/media/preview.77782019.gif"
+  },
+  {
+    "revision": "7ed55111a28c9385901247fffcde57f8",
+    "url": "/static/media/preview.7ed55111.png"
+  },
+  {
+    "revision": "d0fae49ab718f24ef6b154dec4af0dab",
+    "url": "/static/media/preview.d0fae49a.png"
+  },
+  {
+    "revision": "ced8365e41b274e4c304d948591c63ee",
+    "url": "/static/media/star.ced8365e.svg"
+  },
+  {
+    "revision": "76ae75036726fa7648a3e9ec604e632d",
+    "url": "/static/media/up-arrow.76ae7503.svg"
+  }
+]);
